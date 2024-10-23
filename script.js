8ac6687c1a719a66445d8dea778528f7cb923d57
Ragni Pandey <ragni.pandey37@gmail.com>
2024-10-23 08:41:48 +0530

    3 files updated

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+let turn = "X"
+
+//Function to change the turn
+const changeTurn = ()=>{
+    return turn === "X"?"0": "X"
+}
+
+// Function to check for a win
+const checkWin = ()=>{
+
+}
+
+// Game Logic
+let boxes = document.getElementsByClassName("box")
+Array.from(boxes).forEach(element =>{
+    let boxtext = documont.querySelector('.boxtext');
+    element.addEventListener('click', (e)=>{
+        if(e.innerText === ''){
+            e.innerText = turn;
+            changeTurn();audioTurn.play();
+            checkWin();
+            document.getElementsByClassName(turn)[0].innerText = "Turn for " + turn; 
+        }
+    })
+})
